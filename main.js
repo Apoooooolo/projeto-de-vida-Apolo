a0409215b32f3591b5ec904a00d1f571339958c2
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-06-17 07:55:15 -0300

    hxg

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const contadores = document.querySelectorAll(."contador");
-const tempoObjetivo - new Date("2025-10-05t00:00:00");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo = new Date("2025-10-05t00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo - tempoAtual;
