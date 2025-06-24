4bd8368e69880e154da4b98566319384c9ffea68
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-06-24 08:13:24 -0300

    sfsdgf

--- main.js
+++ main.js
@@ -16,6 +16,20 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo = new Date("2025-10-05t00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos =  Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 60;
+
+    return (dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos")
+}
